0ae1dced7911c151dbd070dcbf56a58a3ec3310e
Sreehari Annam <sannam@wescodist.com>
2026-07-03 22:40:07 -0400

    batch 5: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,10 @@
 const floridaCities = [
-  "Orange Park", "Orlando", "Ormond Beach", "Osprey", "Osteen",
-  "Oviedo", "Ozona", "Palatka", "Palm Bay", "Palm Beach",
-  "Palm Beach Gardens", "Palm City", "Palm Coast", "Palm Harbor", "Palmetto",
-  "Panacea", "Panama City", "Panama City Beach", "Parrish", "Pembroke Pines",
-  "Pensacola", "Perry", "Pineland", "Pinellas Park", "Placida",
-  "Plant City", "Polk City", "Pompano Beach", "Ponce De Leon", "Ponte Vedra",
-  "Ponte Vedra Beach", "Port Charlotte", "Port Orange", "Port Richey", "Port Saint Joe",
-  "Port Saint Lucie", "Port Salerno", "Punta Gorda", "Quincy", "Reddick",
-  "Riverview", "Rockledge", "Rotonda West", "Ruskin", "Safety Harbor",
-  "Saint Augustine", "Saint Cloud", "Saint James City", "Saint Johns", "Saint Marks",
-  "Saint Petersburg", "San Antonio", "San Mateo", "Sanford", "Sanibel",
-  "Santa Rosa Beach", "Sarasota", "Satellite Beach", "Sebastian", "Sebring",
-  "Seffner", "Seminole", "Shalimar", "Sharpes", "Silver Springs",
-  "Sorrento", "South Bay", "Spring Hill", "Starke", "Stuart",
-  "Summerfield", "Summerland Key", "Sun City Center", "Sunrise", "Tallahassee",
-  "Tampa", "Tarpon Springs", "Tavares", "Tavernier", "The Villages",
+  "Thonotosassa", "Titusville", "Trenton", "Umatilla", "Valparaiso",
+  "Valrico", "Venice", "Vero Beach", "Wabasso", "Wauchula",
+  "Weirsdale", "Wellington", "Wesley Chapel", "West Palm Beach", "Wewahitchka",
+  "White Springs", "Williston", "Wimauma", "Windermere", "Winter Garden",
+  "Winter Haven", "Winter Park", "Winter Springs", "Woodville", "Yalaha",
+  "Yulee", "Zephyrhills",
 ];
 
 const BATCH_SIZE = 5;
